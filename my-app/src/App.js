056a4e830b6379e0e314ffc4cbb944b016c1127a
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-06-02 04:06:44 -0300

    add a text api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function App() {
+const initiaState = '';
+
+const App = () => {
+  const [film, setFilm] = useState(initiaState);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
+      )
+      .then((res) => setFilm(res.data))
+      .catch((err) => console.log(err));
+    return () => {};
+  }, []);
+
+  console.log(film);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +39,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    caralho
     <App />
   </React.StrictMode>,
   document.getElementById('root')
