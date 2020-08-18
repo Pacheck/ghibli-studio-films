a0a23835087746364f08ed16c1d828ba65c5bcc5
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-17 21:22:01 -0300

    First init aftar coming back to the project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const initiaState = '';
+// Components
+import AppContent from './components/AppContent';
+
+const initiaState = [];
 
 const App = () => {
-  const [film, setFilm] = useState(initiaState);
+  const [films, setFilms] = useState(initiaState);
 
   useEffect(() => {
     axios
-      .get(
-        'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
-      )
-      .then((res) => setFilm(res.data))
+      .get('https://ghibliapi.herokuapp.com/films')
+      .then((res) => setFilms(res.data))
       .catch((err) => console.log(err));
     return () => {};
   }, []);
 
-  console.log(film);
+  console.log(films);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Methods
+
+  return <AppContent />;
 };
 
 export default App;
