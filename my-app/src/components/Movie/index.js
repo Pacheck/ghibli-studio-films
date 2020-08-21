792409ccc73386161e6b34686cef1615ac62b550
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-08-21 14:35:01 -0300

    Movie css and js update

--- my-app/src/components/Movie/index.js
+++ my-app/src/components/Movie/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './index.css';
 
+import Navigator from '../Navigator';
+
 const Movie = ({ movie }) => {
-  return <h2>{movie.title}</h2>;
+  return (
+    <div className="movie-container">
+      <Navigator />
+      <table className="movie-table">
+        <tr>
+          <td>{movie.title}</td>
+          <td>{movie.producer}</td>
+          <td>{movie.releaseDate}</td>
+          <td>{movie.rateScore}</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Movie;
